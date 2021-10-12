b55db32adf9a4a88c0f934a794cb2e431e3c7c7c
Julia Shelton <87336948+jgrmshelton@users.noreply.github.com>
2021-10-11 19:21:54 -0500

    commit

--- routes.js
+++ routes.js
@@ -5,39 +5,39 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-  });
-  
-  router.get('/notetracker', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notetracker.html'));
-  });
-  
-  router.get('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
-    res.json(notetreacker);
-  });
-  
-  router.post('/api/notestracker', (req, res) => {
-    const notetracker = JSON.parse(fs.readFileSync('./db/db.json'));
-    const notetrackerID = Object.assign(req.body, { id: `${uuidv4()}` });
-    notes.push(notetrackerID);
-    const stringNote = JSON.stringify(notetracker);
-    fs.writeFileSync('./db/db.json', stringNote);
-    res.json(notetracker);
-  });
-  
-  router.delete('/api/notetracker/:id', (req, res) => {
-    const notetracker = JSON.parse(fs.readFileSync('./db/db.json'));
-    const notetrackerID = req.params.id;
-    for (let i = 0; i < notetracker.length; i++) {
-      if (notetracker[i].id === notetrackerID) {
-        notetracker.splice(i, 1);
-        const newNote1 = JSON.stringify(notetracker);
-        fs.writeFileSync('./db/db.json', newNote1);
-        return res.json(notes);
-      }
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+router.get('/note', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/note.html'));
+});
+
+router.get('/api/note', (req, res) => {
+  const note = JSON.parse(fs.readFileSync('./db/db.json'));
+  res.json(note);
+});
+
+router.post('/api/note', (req, res) => {
+  const note = JSON.parse(fs.readFileSync('./db/db.json'));
+  const noteID = Object.assign(req.body, { id: `${uuidv4()}` });
+  note.push(noteID);
+  const stringNote = JSON.stringify(note);
+  fs.writeFileSync('./db/db.json', stringNote);
+  res.json(note);
+});
+
+router.delete('/api/note/:id', (req, res) => {
+  const note = JSON.parse(fs.readFileSync('./db/db.json'));
+  const noteID = req.params.id;
+  for (let i = 0; i < note.length; i++) {
+    if (note[i].id === noteID) {
+      note.splice(i, 1);
+      const note1 = JSON.stringify(note);
+      fs.writeFileSync('./db/db.json', note1);
+      return res.json(note);
     }
-  });
-  
-  module.exports = router;
+  }
+});
+
+module.exports = router;
   
